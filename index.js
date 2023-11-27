51a23d9b8e61f50f285b8caf44d6e27375b4a723
apon995 <shofikulsamz995@gmail.com>
2023-11-27 08:13:28 +0600

    create a user collection and empliment requirments task

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const bodyperser = require('body-parser');
@@ -9,10 +10,76 @@ const port = process.env.PORT || 5000;
 
 
 // --middlewares
-app.use(cors())
+app.use(cors({
+    // https://midgenerationcoders.web.app
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(bodyperser.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.38hce1z.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const Servicedb = client.db('MidGenerationCoderDB').collection('Services');
+        const Userdb = client.db('MidGenerationCoderDB').collection('Users')
+
+        app.get('/services', async (req, res) => {
+            const result = await Servicedb.find().toArray();
+
+            res.send(result)
+        })
+
+
+        app.post('/user', async (req, res) => {
+            const user = req?.body;
+            const query = { "user_email": user?.user_email }
+            const exist = await Userdb.findOne(query);
+            if (exist) {
+                return res.send({ message: "Already Exist email !" })
+            }
+            const result = await Userdb.insertOne(user);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+
+        console.log(error)
+
+    }
+}
+run()
 
 
 
